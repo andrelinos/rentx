6e05034bd65f5660fe1854c58f4d518e6a5e56a7
Andrelino Silva <andrelinocom@gmail.com>
2021-11-06 08:51:27 -0300

    Add: Adicionando tema e fontes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+
+import {
+    useFonts,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
+import { Home } from './src/screens/Home';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontsLoaded] = useFonts({
+        Archivo_400Regular,
+        Archivo_500Medium,
+        Inter_400Regular,
+        Inter_500Medium,
+        Archivo_600SemiBold
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
+    return (
+        <>
+            <Home />
+            <StatusBar style="auto" />
+        </>
+    );
 }
