f44ca778098925a2a68bc86f03e6560a72b36b04
Andrelino Silva <andrelinocom@gmail.com>
2021-12-10 08:06:50 -0300

    Try: Tentando alinhar loading... :astronaut:

--- src/components/LoadAnimated/styles.ts
+++ src/components/LoadAnimated/styles.ts
@@ -1,23 +1,40 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
     align-items: center;
-    `;
+`;
+export const LoadingView = styled.View`
+    width: 100%;
+    height: 300px;
+    justify-content: flex-end;
+    align-items: center;
+    overflow: hidden;
+    margin-top: 80px;
+`;
 
 export const Road = styled.View`
-    justify-content: center;
-    align-items: center;
-    max-height: 180px;
-    position: absolute;
-    z-index: -1;
-    top: 90px;
+    position: relative;
+    width: 100%;
+    height: 190px;
+    margin-bottom: ${RFValue(150)}px;
 `;
 
 export const Car = styled.View`
-    justify-content: center;
+    width: 100%;
+    justify-content: flex-end;
     align-items: center;
     position: absolute;
-    top: 190px;
+    height: 190px;
+    bottom: ${RFValue(-15)}px;
+    z-index: 9;
+`;
+
+export const LoadText = styled.Text`
+    width: 100%;
+    font-family: ${({ theme }) => theme.fonts.secondary_400};
+    color: ${({ theme }) => theme.colors.main};
+    font-size: ${RFValue(15)}px;
+    text-align: center;
 `;
