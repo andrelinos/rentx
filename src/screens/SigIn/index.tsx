f34492cff5e85bd8024cef85e00f6852663ec737
Andrelino Silva <andrelinocom@gmail.com>
2021-12-17 08:16:33 -0300

    Add: Acessibilidade ao preencher os campos

--- src/screens/SigIn/index.tsx
+++ src/screens/SigIn/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import {
+    StatusBar,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native';
 import { useTheme } from 'styled-components';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -11,51 +16,56 @@ export function SigIn() {
     const theme = useTheme();
 
     return (
-        <Container>
-            <StatusBar
-                barStyle="dark-content"
-                backgroundColor="transparent"
-                translucent
-            />
-            <Header>
-                <Title>Estamos{'\n'}quase lá.</Title>
-                <SubTitle>
-                    Faça seu login para começar{'\n'}uma experiência incrível.
-                </SubTitle>
-            </Header>
+        <KeyboardAvoidingView behavior="position" enabled>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <Container>
+                    <StatusBar
+                        barStyle="dark-content"
+                        backgroundColor={theme.colors.background_primary}
+                        translucent
+                    />
+                    <Header>
+                        <Title>Estamos{'\n'}quase lá.</Title>
+                        <SubTitle>
+                            Faça seu login para começar{'\n'}uma experiência
+                            incrível.
+                        </SubTitle>
+                    </Header>
 
-            <Form>
-                <Input
-                    iconName="mail"
-                    placeholder="E-mail"
-                    keyboardType="email-address"
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                />
-                <PasswordInput
-                    iconName="lock"
-                    placeholder="Senha"
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                />
-            </Form>
+                    <Form>
+                        <Input
+                            iconName="mail"
+                            placeholder="E-mail"
+                            keyboardType="email-address"
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            />
 
-            <Footer>
-                <Button
-                    title="Login"
-                    onPress={() => {}}
-                    enabled={false}
-                    loading={false}
-                />
-                <Button
-                    title="Criar conta gratuita"
-                    color={theme.colors.background_secondary}
-                    onPress={() => {}}
-                    enabled={false}
-                    loading={false}
-                    light
-                />
-            </Footer>
-        </Container>
+                        <PasswordInput
+                            iconName="lock"
+                            placeholder="Senha"
+                            autoCapitalize="none"
+                        />
+                    </Form>
+
+                    <Footer>
+                        <Button
+                            title="Login"
+                            onPress={() => {}}
+                            enabled={false}
+                            loading={false}
+                        />
+                        <Button
+                            title="Criar conta gratuita"
+                            color={theme.colors.background_secondary}
+                            onPress={() => {}}
+                            enabled={false}
+                            loading={false}
+                            light
+                        />
+                    </Footer>
+                </Container>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 }
